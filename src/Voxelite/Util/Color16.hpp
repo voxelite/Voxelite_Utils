31789899e8918f7337c5e31d4f9fcd0db8aa24eb
Abit Gray <AbitTheGray@gmail.com>
2020-12-06 17:57:43 +0100

    New Color16 implementation Not a class but group of utility functions + predefined colors

--- src/Voxelite/Util/Color16.hpp
+++ src/Voxelite/Util/Color16.hpp
@@ -3,214 +3,99 @@
 #include <cstdint>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
-#include <ostream>
-#include <sstream>
 
 namespace Voxelite::Util
 {
-    class Color
+    typedef uint16_t color16_t;
+
+    [[nodiscard]] inline constexpr glm::vec3 AsColorRGB(color16_t value) noexcept
+    {
+        return glm::vec3(
+                static_cast<float>(static_cast<uint8_t>(value >> 0u) & 0x0Fu) / 15.0f,
+                static_cast<float>(static_cast<uint8_t>(value >> 4u) & 0x0Fu) / 15.0f,
+                static_cast<float>(static_cast<uint8_t>(value >> 8u) & 0x0Fu) / 15.0f
+                        );
+    }
+    [[nodiscard]] inline constexpr glm::vec4 AsColorRGBA(color16_t value) noexcept
+    {
+        return glm::vec4(
+                static_cast<float>(static_cast<uint8_t>(value >>  0u) & 0x0Fu) / 15.0f,
+                static_cast<float>(static_cast<uint8_t>(value >>  4u) & 0x0Fu) / 15.0f,
+                static_cast<float>(static_cast<uint8_t>(value >>  8u) & 0x0Fu) / 15.0f,
+                static_cast<float>(static_cast<uint8_t>(value >> 12u) & 0x0Fu) / 15.0f
+                        );
+    }
+    [[nodiscard]] inline constexpr color16_t FromColor(float r, float g, float b) noexcept
+    {
+        assert(r >= 0.0f);
+        assert(r <= 1.0f);
+
+        assert(g >= 0.0f);
+        assert(g <= 1.0f);
+
+        assert(b >= 0.0f);
+        assert(b <= 1.0f);
+
+        return (static_cast<uint16_t>(r * 15.0f) << 0u) +
+               (static_cast<uint16_t>(g * 15.0f) << 4u) +
+               (static_cast<uint16_t>(b * 15.0f) << 8u);
+    }
+    [[nodiscard]] inline color16_t FromColor(const glm::vec3& rgb) noexcept
+    {
+        return FromColor(rgb.r, rgb.g, rgb.b);
+    }
+
+    [[nodiscard]] inline constexpr color16_t FromColor(float r, float g, float b, float a) noexcept
+    {
+        assert(r >= 0.0f);
+        assert(r <= 1.0f);
+
+        assert(g >= 0.0f);
+        assert(g <= 1.0f);
+
+        assert(b >= 0.0f);
+        assert(b <= 1.0f);
+
+        assert(a >= 0.0f);
+        assert(a <= 1.0f);
+
+        return (static_cast<uint16_t>(r * 15.0f) << 0u) +
+               (static_cast<uint16_t>(g * 15.0f) << 4u) +
+               (static_cast<uint16_t>(b * 15.0f) << 8u) +
+               (static_cast<uint16_t>(a * 15.0f) << 12u);
+    }
+    [[nodiscard]] inline color16_t FromColor(const glm::vec4& rgba) noexcept
+    {
+        return FromColor(rgba.r, rgba.g, rgba.b, rgba.a);
+    }
+
+    class Color16
     {
     public:
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "google-explicit-constructor"
-        constexpr Color(uint16_t color) noexcept : m_Color(color)
-        {
-        }
+        Color16() = delete; // Static class
 
-        inline constexpr Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) noexcept
-         : Color(
-                 (static_cast<uint16_t>(alpha / 17u) << 12u) +
-                 (static_cast<uint16_t>(blue / 17u) << 8u) +
-                 (static_cast<uint16_t>(green / 17u) << 4u) +
-                 (static_cast<uint16_t>(red / 17u) << 0u)
-                )
-        {
-        }
+        // Example colors:
 
-        inline constexpr Color(uint8_t red, uint8_t green, uint8_t blue) noexcept
-         : Color(red, green, blue, 0u)
-        {
-        }
-
-        inline constexpr Color(glm::ivec4 vec) noexcept
-         : Color(
-                 static_cast<uint8_t>(vec.r),
-                 static_cast<uint8_t>(vec.g),
-                 static_cast<uint8_t>(vec.b),
-                 static_cast<uint8_t>(vec.a)
-                )
-        {
-        }
-
-        inline constexpr Color(glm::ivec3 vec) noexcept
-         : Color(
-                 static_cast<uint8_t>(vec.r),
-                 static_cast<uint8_t>(vec.g),
-                 static_cast<uint8_t>(vec.b)
-                )
-        {
-        }
-
-        inline constexpr Color(float red, float green, float blue, float alpha) noexcept
-         : Color(
-                 static_cast<uint8_t>(red * 255),
-                 static_cast<uint8_t>(green * 255),
-                 static_cast<uint8_t>(blue * 255),
-                 static_cast<uint8_t>(alpha * 255)
-                )
-        {
-        }
-
-        inline constexpr Color(float red, float green, float blue) noexcept
-         : Color(red, green, blue, 0.0f)
-        {
-        }
-
-        inline constexpr Color(glm::vec4 vec) noexcept
-         : Color(
-                 vec.r,
-                 vec.g,
-                 vec.b,
-                 vec.a
-                )
-        {
-        }
-
-        inline constexpr Color(glm::vec3 vec) noexcept
-         : Color(
-                 vec.r,
-                 vec.g,
-                 vec.b
-                )
-        {
-        }
-#pragma clang diagnostic pop
-
-    private:
-        const uint16_t m_Color;
-
-    // Bytes
+        // Transparent
     public:
-        [[nodiscard]] inline uint8_t Red() const noexcept
-        {
-            return (static_cast<uint8_t>(m_Color >> 0u) & 0x000Fu) * 17;
-        }
-
-        [[nodiscard]] inline uint8_t Green() const noexcept
-        {
-            return (static_cast<uint8_t>(m_Color >> 4u) & 0x000Fu) * 17;
-        }
-
-        [[nodiscard]] inline uint8_t Blue() const noexcept
-        {
-            return (static_cast<uint8_t>(m_Color >> 8u) & 0x000Fu) * 17;
-        }
-
-        [[nodiscard]] inline uint8_t Alpha() const noexcept
-        {
-            return (static_cast<uint8_t>(m_Color >> 12u) & 0x000Fu) * 17;
-        }
-
-    // Vectors
+        static const color16_t Transparent_White = FromColor(1.0f, 1.0f, 1.0f, 1.0f);
+        static const color16_t Transparent_Black = FromColor(0.0f, 0.0f, 0.0f, 1.0f);
+        // Grayscale
     public:
-        [[nodiscard]] inline glm::vec3 AsVector3() const noexcept
-        {
-            return glm::vec3(
-                    static_cast<float>(Red()) / 255.0f,
-                    static_cast<float>(Green()) / 255.0f,
-                    static_cast<float>(Blue()) / 255.0f
-                    );
-        }
-        [[nodiscard]] inline glm::vec4 AsVector4() const noexcept
-        {
-            return glm::vec4(
-                    static_cast<float>(Red()) / 255.0f,
-                    static_cast<float>(Green()) / 255.0f,
-                    static_cast<float>(Blue()) / 255.0f,
-                    static_cast<float>(Alpha()) / 255.0f
-                    );
-        }
-        [[nodiscard]] inline glm::ivec3 AsIntVector3() const noexcept
-        {
-            return glm::ivec3(
-                    Red(),
-                    Green(),
-                    Blue()
-                    );
-        }
-        [[nodiscard]] inline glm::ivec4 AsIntVector4() const noexcept
-        {
-            return glm::ivec4(
-                    Red(),
-                    Green(),
-                    Blue(),
-                    Alpha()
-                    );
-        }
+        static const color16_t White = FromColor(1.0f, 1.0f, 1.0f);
+        static const color16_t Gray  = FromColor(0.5f, 0.5f, 0.5f);
+        static const color16_t Black = FromColor(0.0f, 0.0f, 0.0f);
 
+        // RGB components
     public:
-        [[nodiscard]] inline std::string AsConsoleCode() const
-        {
-            std::stringstream ss;
-            ss << "\E[38;2;" << Red() << ';' << Green() << ';' << Blue() << 'm';
-            return ss.str();
-        }
+        static const color16_t Red   = FromColor(1.0f, 0.0f, 0.0f);
+        static const color16_t Green = FromColor(0.0f, 1.0f, 0.0f);
+        static const color16_t Blue  = FromColor(0.0f, 0.0f, 1.0f);
 
-    private:
-        static constexpr const char* decToHex = "0123456789ABCDEF";
+        // RGB 2-components
     public:
-        [[nodiscard]] inline std::string AsHexRGB() const noexcept
-        {
-            char hexArray[8] = {
-                    '#',
-                    'R', 'R',
-                    'G', 'G',
-                    'B', 'B',
-                    '\0'
-            };
-
-            uint8_t red = Red();
-            hexArray[1] = decToHex[static_cast<uint8_t>(red >> 0u) & 0x0Fu];
-            hexArray[2] = decToHex[static_cast<uint8_t>(red >> 4u) & 0x0Fu];
-
-            uint8_t green = Green();
-            hexArray[3] = decToHex[static_cast<uint8_t>(green >> 0u) & 0x0Fu];
-            hexArray[4] = decToHex[static_cast<uint8_t>(green >> 4u) & 0x0Fu];
-
-            uint8_t blue = Blue();
-            hexArray[5] = decToHex[static_cast<uint8_t>(blue >> 0u) & 0x0Fu];
-            hexArray[6] = decToHex[static_cast<uint8_t>(blue >> 4u) & 0x0Fu];
-
-            return std::string(hexArray);
-        }
-        [[nodiscard]] inline std::string AsHexRGBA() const noexcept
-        {
-            char hexArray[10] = {
-                    '#',
-                    'R', 'R',
-                    'G', 'G',
-                    'B', 'B',
-                    'A', 'A',
-                    '\0'
-            };
-
-            uint8_t red = Red();
-            hexArray[2] = decToHex[static_cast<uint8_t>(red >> 0u) & 0x0Fu];
-            hexArray[1] = decToHex[static_cast<uint8_t>(red >> 4u) & 0x0Fu];
-
-            uint8_t green = Green();
-            hexArray[4] = decToHex[static_cast<uint8_t>(green >> 0u) & 0x0Fu];
-            hexArray[3] = decToHex[static_cast<uint8_t>(green >> 4u) & 0x0Fu];
-
-            uint8_t blue = Blue();
-            hexArray[6] = decToHex[static_cast<uint8_t>(blue >> 0u) & 0x0Fu];
-            hexArray[5] = decToHex[static_cast<uint8_t>(blue >> 4u) & 0x0Fu];
-
-            uint8_t alpha = Alpha();
-            hexArray[8] = decToHex[static_cast<uint8_t>(alpha >> 0u) & 0x0Fu];
-            hexArray[7] = decToHex[static_cast<uint8_t>(alpha >> 4u) & 0x0Fu];
-
-            return std::string(hexArray);
-        }
+        static const color16_t Yellow = FromColor(1.0f, 1.0f, 0.0f);
+        static const color16_t Aqua   = FromColor(0.0f, 1.0f, 1.0f);
+        static const color16_t Purple = FromColor(1.0f, 0.0f, 1.0f);
     };
 }
